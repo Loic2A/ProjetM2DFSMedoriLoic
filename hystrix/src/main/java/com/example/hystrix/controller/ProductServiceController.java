e8c86f8dbcd46783f5f18b72e9de94034582c55f
Loic2A <lomedori.15@gmail.com>
2020-10-23 14:12:39 +0200

    Add microservice

---
+++ hystrix/src/main/java/com/example/hystrix/controller/ProductServiceController.java
@@ -0,0 +1,4 @@
+package com.example.hystrix.controller;
+
+public class ProductServiceController {
+}
---
+++ hystrix/src/main/java/com/example/hystrix/delegate/ProductServiceDelegate.java
@@ -0,0 +1,4 @@
+package com.example.hystrix.delegate;
+
+public class ProductServiceDelegate {
+}
